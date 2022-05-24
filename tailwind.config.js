f13a196cf0839fd7b465e12e7de665105f41fc05
Hector Lopez <hlopezcarballo@gmail.com>
2022-05-23 19:46:04 -0500

    Add Raleway font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        Raleway: "Raleway, sans-serif",
+      },
       colors: {
         white: {
           custom: "#E7E7EB",
