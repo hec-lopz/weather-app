f9a430d6115cdcf99da2c6921b8a099cfa169fa2
Hector Lopez <hlopezcarballo@gmail.com>
2022-06-01 11:50:36 -0500

    Add App UI containers and Weather logic
    Created containers for the UI to be better structured. Added the logic
    to fetch the Weather Data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Header from "./components/Header";
 import Highlights from "./components/Highlights";
 import CurrentWeather from "./components/CurrentWeather";
 import FutureWeather from "./components/FutureWeather";
+import Layout from "./containers/Layout";
+import Main from "./containers/MainSection";
+import Details from "./containers/DetailsSection";
+import Footer from "./containers/Footer";
 
+type WeatherData = null | {
+  lat: number;
+  lon: number;
+  timezone: string;
+  current?: Object;
+  minutely?: Object;
+  hourly?: Object;
+  daily?: Object;
+  alerts?: Object;
+};
 function App() {
+  const API = "https://api.openweathermap.org/data/3.0/onecall";
+  const [location, setLocation] = useState({ lat: 19.3494, lon: -99.1935 });
+  const [weatherData, setWeatherData] = useState({ current: null });
+  useEffect(() => {
+    //debugger;
+    console.log("Effect");
+    axios
+      .get(API, {
+        params: {
+          lat: location.lat,
+          lon: location.lon,
+          exclude: "minutely,hourly,alerts",
+          units: "metric",
+          appid: "a5cc4021b7947e9e101839ca1270a161",
+        },
+      })
+      .then((res) => {
+        console.log("data", res.data);
+        setWeatherData(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, [location.lat, location.lon]);
+
   return (
-    <>
-      <Header />
-      <CurrentWeather />
-      <FutureWeather />
-      <Highlights />
-    </>
+    <Layout>
+      <Main>
+        <Header />
+        <CurrentWeather currentData={weatherData.current} location={location} />
+      </Main>
+      <Details>
+        <FutureWeather />
+        <Highlights />
+        <Footer />
+      </Details>
+    </Layout>
   );
 }
 
