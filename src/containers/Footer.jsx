db1b63a6fdbfa5ab3a31059997f437cc12f9ae25
Hector Lopez <hlopezcarballo@gmail.com>
2022-06-07 11:25:39 -0500

    Add icon attribution

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,9 +1,15 @@
 const Footer = () => (
   <footer className="text-white-custom flex items-center justify-center flex-col p-10">
     <p>Coded by hec_lopz - devchallenges.io</p>
-    <p>
+    <p className="flex flex-col justify-center items-center">
       <a href="https://www.flaticon.com/free-icons/fog" title="fog icons">
         Fog icons created by Freepik - Flaticon
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/weather"
+        title="weather icons"
+      >
+        Broken clouds icon created by kosonicon - Flaticon
       </a>
     </p>
   </footer>
