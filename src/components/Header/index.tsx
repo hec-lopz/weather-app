a2ee7c19d163f9200f7ee3c694a2bd801ce76071
Hector Lopez <hlopezcarballo@gmail.com>
2022-06-07 14:08:31 -0500

    Add element css states

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,10 @@ const Header = () => {
   return (
     <header className="bg-secondary  flex items-center px-3 py-4 ">
       <nav className="min-h-min w-full flex justify-between ">
-        <button className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-40 h-10 bg-grayBtn text-white-custom">
+        <button className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-40 h-10 bg-grayBtn text-white-custom hover:bg-zinc-600 ">
           Search for places
         </button>
-        <button className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-full bg-grayBtn  w-10 h-10 grid place-items-center text-white-custom">
+        <button className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-full bg-grayBtn  w-10 h-10 grid place-items-center text-white-custom hover:bg-zinc-600">
           <span className="material-icons">my_location</span>
         </button>
       </nav>
--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,13 +1,33 @@
 const Footer = () => (
   <footer className="text-white-custom flex items-center justify-center flex-col p-10">
-    <p>Coded by hec_lopz - devchallenges.io</p>
+    <p>
+      Coded by{" "}
+      <a
+        className="font-bold underline visited:text-blue-700 hover:text-gray-500"
+        href="https://github.com/hec-lopz"
+        target="_blank"
+        rel="noreferrer"
+      >
+        hec_lopz{" "}
+      </a>{" "}
+      - devchallenges.io
+    </p>
     <p className="flex flex-col justify-center items-center">
-      <a href="https://www.flaticon.com/free-icons/fog" title="fog icons">
+      <a
+        className="font-bold underline visited:text-blue-700 hover:text-gray-500"
+        href="https://www.flaticon.com/free-icons/fog"
+        title="fog icons"
+        target="_blank"
+        rel="noreferrer"
+      >
         Fog icons created by Freepik - Flaticon
       </a>
       <a
+        className="font-bold underline visited:text-blue-700 hover:text-gray-500"
         href="https://www.flaticon.com/free-icons/weather"
         title="weather icons"
+        target="_blank"
+        rel="noreferrer"
       >
         Broken clouds icon created by kosonicon - Flaticon
       </a>
